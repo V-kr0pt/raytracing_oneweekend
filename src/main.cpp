129c36825d74928ed5ee26c8c440c5238e3d1ba3
Vitor <vitor.sousa.franca@gmail.com>
2024-11-05 21:55:10 -0300

    fix: viewport_width (inverted fraction) + add: hit_sphere function + ray_color returning red when the ray hits the sphere

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,24 @@
 #include "ray.h"
 #include <iostream> 
 
+bool hit_sphere(const point3& center, double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0*dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
+}
 
 color ray_color(const ray& r){
+    point3 sphere_center(0, 0, -1);
+    if (hit_sphere(sphere_center, 0.5, r)){
+        return color(1, 0, 0);
+    }
+
+
     vec3 unit_direction = unit_vector(r.direction());
     auto alpha = 0.5*(unit_direction.y()+1); //unit_direction is in the range -1 to 1, alpha has to be in the range 0 to 1
-    std::clog << "alpha: " << alpha << std::endl;
     return (1-alpha)*color(1, 1, 1) + alpha*color(0, 0, 1);
 }
 
@@ -19,9 +32,9 @@ int main(){
     height_img = (height_img < 1) ? 1 : height_img;
 
     // Camera
-    auto focal_length = 1.0;
+    auto focal_length = 1;
     auto viewport_height = 2.0;
-    auto viewport_width = (height_img/width_img) * viewport_height; // it can be different from aspect_ratio
+    auto viewport_width = viewport_height*(double(width_img)/height_img); // it can be different from aspect_ratio
     auto camera_center = point3(0, 0, 0);
 
     // vectors from the viewport
@@ -33,7 +46,7 @@ int main(){
     auto pixel_delta_u = viewport_u / width_img;
 
     // Calculate the location of the upper left pixel.
-    auto upper_left_corner = camera_center - viewport_u/2 - viewport_v/2 + vec3(0,0,focal_length);
+    auto upper_left_corner = camera_center - viewport_u/2 - viewport_v/2 - vec3(0,0,focal_length);
     auto pixel00_loc = upper_left_corner + 0.5*(pixel_delta_u+pixel_delta_v);
 
 

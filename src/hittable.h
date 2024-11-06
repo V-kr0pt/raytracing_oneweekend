457374d18a52ccfdd22e3657e0b190dd27d211b4
Vitor <vitor.sousa.franca@gmail.com>
2024-11-06 15:59:25 -0300

    add: front faces and back faces normal

--- src/hittable.h
+++ src/hittable.h
@@ -7,6 +7,13 @@ class hit_record {
         point3 p;
         vec3 normal;
         double t;
+        bool front_face;
+
+        void set_face_normal(const ray& r, const vec3& outward_normal){
+            front_face = dot(r.direction(), outward_normal) < 0; // if the dot product is negative, the normal is pointing in opposite direction of the ray
+            normal = front_face ? outward_normal : -outward_normal;
+        }
+
 };
 
 class hittable{

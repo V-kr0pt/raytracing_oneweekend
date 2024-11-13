2c6e51e10fca58ac83b0f127e2a80c8cad09727a
Vitor <vitor.sousa.franca@gmail.com>
2024-11-13 18:16:15 -0300

    add: Lambertian reflection

--- src/camera.h
+++ src/camera.h
@@ -65,7 +65,7 @@ class camera {
             if (depth <= 0){ return color(0,0,0); }
             
             if (world.hit(r, 0.001, infinity, rec)){
-                vec3 direction = random_vector_on_hemisphere(rec.normal);
+                vec3 direction = rec.normal + random_vector_on_hemisphere(rec.normal);
                 ray ray_reflex = ray(rec.p, direction);
                 return 0.5*ray_color(ray_reflex, depth-1, world); // Recursion
             }

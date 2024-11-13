75c994241b06d975c338f2be651217d9756640f2
Vitor <vitor.sousa.franca@gmail.com>
2024-11-13 18:21:46 -0300

    add: maping colors to gamma2 space

--- src/color.h
+++ src/color.h
@@ -6,10 +6,23 @@
 
 using color = vec3;
 
+//transform linear components to gamma2 space of colors
+inline double linear_to_gamma(double linear_comp){
+    if (linear_comp > 0){
+        return std::sqrt(linear_comp);
+    }
+    return 0;
+}
+
+
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     static const interval intensity(0.0, 0.999);
     // Translate the [0,1] component values to the byte range [0,255].
